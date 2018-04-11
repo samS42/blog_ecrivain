3f9e23f59444be92bcfa540db742cf1adba7be9e
samS42 <s.a.m.4.2@hotmail.fr>
2018-04-11 14:27:38 +0200

    Ajout de la sécurité pour les pages d'administration

--- index.php
+++ index.php
@@ -73,148 +73,6 @@ try
 			}
 		}
 
-		/*Display titles in admin's page*/
-		elseif ($_GET['action'] == 'displayTitles')
-		{
-			listTitles();
-		}
-
-		/*Go to selected post in admin's page*/
-		elseif ($_GET['action'] == 'adminPost')
-		{
-			if(isset($_GET['id']) AND $_GET['id']>0)
-			{
-				$_GET['id'] = (int) $_GET['id'];
-				displayPost(htmlspecialchars($_GET['id']));
-			}
-		}
-
-		/*Delete selected post*/
-		elseif (isset($_GET['idPost']) AND $_GET['idPost']>0)
-		{
-			$_GET['idPost'] = (int) $_GET['idPost'];
-
-			if ($_GET['action'] == 'deletePost')
-			{
-				deletePost(htmlspecialchars($_GET['idPost']));
-			}
-			else
-			{
-				throw new Exception('Toutes les données n\'ont pas été renseignées');
-			}
-		}
-
-		/*Delete selected comment*/
-		elseif ($_GET['action'] == 'deleteComment')
-		{
-			if(isset($_GET['idComment']) AND $_GET['idComment']>0 AND isset($_GET['id']) AND $_GET['id']>0)
-			{
-				$_GET['idComment'] = (int) $_GET['idComment'];
-				$_GET['id'] = (int) $_GET['id'];
-				deleteComment(htmlspecialchars($_GET['idComment']), htmlspecialchars($_GET['id']));
-			}
-			else
-			{
-				throw new Exception('Toutes les données n\'ont pas été renseignées');
-			}
-		}
-
-		/*Delete signaled comment*/
-		elseif ($_GET['action'] == 'adminDeleteComment')
-		{
-			if(isset($_GET['idComment']) AND $_GET['idComment']>0 AND isset($_GET['id']) AND $_GET['id']>0)
-			{
-				$_GET['idComment'] = (int) $_GET['idComment'];
-				$_GET['id'] = (int) $_GET['id'];
-				deleteCommentSignaled(htmlspecialchars($_GET['idComment']), htmlspecialchars($_GET['id']));
-			}
-			else
-			{
-				throw new Exception('Toutes les données n\'ont pas été renseignées');
-			}
-		}
-
-		/*Add post*/
-		elseif ($_GET['action'] == 'addPost')
-		{
-			if(!empty($_POST['myTitle']) AND !empty($_POST['myTextarea']))
-			{
-				addPost($_POST['myTitle'], $_POST['myTextarea']);
-			}
-			else
-			{
-				throw new Exception('Touts les champs n\'ont pas été renseignées');
-			}
-		}
-
-		/*Display form update*/
-		elseif ($_GET['action'] == 'displayUpdatePost')
-		{
-			if (isset($_GET['id']) AND is_numeric($_GET['id']))
-			{
-				displayUpdatePost(htmlspecialchars($_GET['id']));
-			}
-			else
-			{
-				throw new Exception('Touts les champs n\'ont pas été renseignées');
-			}
-		}
-
-		/*Update post*/
-		elseif ($_GET['action'] == 'updatePost')
-		{
-			if (isset($_GET['id']) AND $_GET['id']>0 AND isset($_POST['myTitle']) AND isset($_POST['myTextarea']))
-			{
-				$_GET['id'] = (int) $_GET['id'];
-				updatePost(htmlspecialchars($_GET['id']), $_POST['myTitle'], $_POST['myTextarea']);
-			}
-			else
-			{
-				throw new Exception('Touts les champs n\'ont pas été renseignées');
-			}
-		}
-
-		/*User signal comment*/
-		elseif ($_GET['action'] == 'signalComment')
-		{
-			if(isset($_GET['idComment']) AND $_GET['idComment']>0 AND isset($_GET['id']) AND $_GET['id']>0)
-			{
-				$_GET['idComment'] = (int) $_GET['idComment'];
-				$_GET['id'] = (int) $_GET['id'];
-				signalComment(htmlspecialchars($_GET['idComment']), htmlspecialchars($_GET['id']));
-			}
-			else
-			{
-				throw new Exception('Le signalement n\' pas été effectué');
-			}
-		}
-
-		/*Display add post*/
-		elseif ($_GET['action'] == 'displayAddPost')
-		{
-			displayAddPost();
-		}
-
-		/*Display signaled comment*/
-		elseif ($_GET['action'] == 'displaySignalComment')
-		{
-			displaySignalComment();
-		}
-
-		/*Signal deleted but the comment is still visible on the post*/
-		elseif ($_GET['action'] == 'deleteSignal')
-		{
-			if(isset($_GET['idComment']) AND $_GET['idComment']>0)
-			{
-				$_GET['idComment'] = (int) $_GET['idComment'];
-				deleteSignalComment(htmlspecialchars($_GET['idComment']));
-			}
-			else
-			{
-				throw new Exception('Le signalement n\' pas été supprimé');
-			}
-		}
-
 		/*Come back to listPostsView.php*/
 		elseif ($_GET['action'] == 'index')
 		{
@@ -226,6 +84,155 @@ try
 			{
 				listPosts();
 			}
+		}
+
+		elseif(isset($_SESSION['pseudo']))
+		{
+			/*Display titles in admin's page*/
+			if ($_GET['action'] == 'displayTitles')
+			{
+				listTitles();
+			}
+
+			/*Go to selected post in admin's page*/
+			elseif ($_GET['action'] == 'adminPost')
+			{
+				if(isset($_GET['id']) AND $_GET['id']>0)
+				{
+					$_GET['id'] = (int) $_GET['id'];
+					displayPost(htmlspecialchars($_GET['id']));
+				}
+			}
+
+			/*Delete selected post*/
+			elseif (isset($_GET['idPost']) AND $_GET['idPost']>0)
+			{
+				$_GET['idPost'] = (int) $_GET['idPost'];
+
+				if ($_GET['action'] == 'deletePost')
+				{
+					deletePost(htmlspecialchars($_GET['idPost']));
+				}
+				else
+				{
+					throw new Exception('Toutes les données n\'ont pas été renseignées');
+				}
+			}
+
+			/*Delete selected comment*/
+			elseif ($_GET['action'] == 'deleteComment')
+			{
+				if(isset($_GET['idComment']) AND $_GET['idComment']>0 AND isset($_GET['id']) AND $_GET['id']>0)
+				{
+					$_GET['idComment'] = (int) $_GET['idComment'];
+					$_GET['id'] = (int) $_GET['id'];
+					deleteComment(htmlspecialchars($_GET['idComment']), htmlspecialchars($_GET['id']));
+				}
+				else
+				{
+					throw new Exception('Toutes les données n\'ont pas été renseignées');
+				}
+			}
+
+			/*Delete signaled comment*/
+			elseif ($_GET['action'] == 'adminDeleteComment')
+			{
+				if(isset($_GET['idComment']) AND $_GET['idComment']>0 AND isset($_GET['id']) AND $_GET['id']>0)
+				{
+					$_GET['idComment'] = (int) $_GET['idComment'];
+					$_GET['id'] = (int) $_GET['id'];
+					deleteCommentSignaled(htmlspecialchars($_GET['idComment']), htmlspecialchars($_GET['id']));
+				}
+				else
+				{
+					throw new Exception('Toutes les données n\'ont pas été renseignées');
+				}
+			}
+
+			/*Add post*/
+			elseif ($_GET['action'] == 'addPost')
+			{
+				if(!empty($_POST['myTitle']) AND !empty($_POST['myTextarea']))
+				{
+					addPost($_POST['myTitle'], $_POST['myTextarea']);
+				}
+				else
+				{
+					throw new Exception('Touts les champs n\'ont pas été renseignées');
+				}
+			}
+
+			/*Display form update*/
+			elseif ($_GET['action'] == 'displayUpdatePost')
+			{
+				if (isset($_GET['id']) AND is_numeric($_GET['id']))
+				{
+					displayUpdatePost(htmlspecialchars($_GET['id']));
+				}
+				else
+				{
+					throw new Exception('Touts les champs n\'ont pas été renseignées');
+				}
+			}
+
+			/*Update post*/
+			elseif ($_GET['action'] == 'updatePost')
+			{
+				if (isset($_GET['id']) AND $_GET['id']>0 AND isset($_POST['myTitle']) AND isset($_POST['myTextarea']))
+				{
+					$_GET['id'] = (int) $_GET['id'];
+					updatePost(htmlspecialchars($_GET['id']), $_POST['myTitle'], $_POST['myTextarea']);
+				}
+				else
+				{
+					throw new Exception('Touts les champs n\'ont pas été renseignées');
+				}
+			}
+
+			/*User signal comment*/
+			elseif ($_GET['action'] == 'signalComment')
+			{
+				if(isset($_GET['idComment']) AND $_GET['idComment']>0 AND isset($_GET['id']) AND $_GET['id']>0)
+				{
+					$_GET['idComment'] = (int) $_GET['idComment'];
+					$_GET['id'] = (int) $_GET['id'];
+					signalComment(htmlspecialchars($_GET['idComment']), htmlspecialchars($_GET['id']));
+				}
+				else
+				{
+					throw new Exception('Le signalement n\' pas été effectué');
+				}
+			}
+
+			/*Display add post*/
+			elseif ($_GET['action'] == 'displayAddPost')
+			{
+				displayAddPost();
+			}
+
+			/*Display signaled comment*/
+			elseif ($_GET['action'] == 'displaySignalComment')
+			{
+				displaySignalComment();
+			}
+
+			/*Signal deleted but the comment is still visible on the post*/
+			elseif ($_GET['action'] == 'deleteSignal')
+			{
+				if(isset($_GET['idComment']) AND $_GET['idComment']>0)
+				{
+					$_GET['idComment'] = (int) $_GET['idComment'];
+					deleteSignalComment(htmlspecialchars($_GET['idComment']));
+				}
+				else
+				{
+					throw new Exception('Le signalement n\' pas été supprimé');
+				}
+			}
+		}
+		else
+		{
+			listPosts();
 		}
 	}
 	
